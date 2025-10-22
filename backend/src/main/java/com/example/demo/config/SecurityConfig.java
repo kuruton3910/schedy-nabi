e25e11058a0ff799c5d4b9dc5d73e0b36966bc4b
クルトン <beckeymiku@gmail.com>
2025-10-23 00:57:53 +0900

    ooodd

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -44,7 +44,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+       configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://schedynabi.kitzu.net",
+                "https://schedy-nabi-oqgl.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
